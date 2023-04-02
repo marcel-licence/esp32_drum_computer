2fec465a4de7483a480c82b713d16696441845f9
Marcel Licence <marcel.licence@gmx.de>
2023-04-02 16:27:29 +0200

    Use single inline file

--- esp32_drum_computer.ino
+++ esp32_drum_computer.ino
@@ -66,16 +66,7 @@
 #include <ml_types.h>
 
 #define ML_SYNTH_INLINE_DECLARATION
-#include <audio_module.h>
-#include <blink.h>
-#include <es8388.h>
-#include <esp32_audio_kit_module.h>
-#include <i2s_interface.h>
-#include <midi_interface.h>
-#include <midi_stream_player.h>
-#include <i2s_module.h>
-#include <midi_via_ble.h>
-#include <midi_via_usb.h>
+#include <ml_inline.h>
 #undef ML_SYNTH_INLINE_DECLARATION
 
 void setup()
